91653f6d87cb38ac53fb512d4d5ee1282f15abec
AADITYA26 <dhannawataaditya@gmail.com>
2025-03-15 12:10:32 +0530

    Update dist_subcriber.py

--- dist_subcriber.py
+++ dist_subcriber.py
@@ -7,28 +7,23 @@ import math
 class DistanceCalculator(Node):
     def __init__(self):
         super().__init__('distance_calculator')
-        self.create_subscriber()
-        self.create_publisher()
         self.x = 0.0
         self.y = 0.0
-    
-    def create_subscriber(self):
+        self.distance = 0.0
         self.subscription = self.create_subscription(Pose, '/turtle1/pose', self.pose_callback, 10)
-    
-    def create_publisher(self):
         self.publisher_ = self.create_publisher(Float32, '/turtle1/distance_from_origin', 10)
+        self.timer = self.create_timer(0.5, self.publish_distance)
     
     def pose_callback(self, msg):
         self.x = msg.x
         self.y = msg.y
-        self.publish_distance()
+        self.distance = math.sqrt(self.x**2 + self.y**2)
     
     def publish_distance(self):
-        distance = math.sqrt(self.x**2 + self.y**2)
         distance_msg = Float32()
-        distance_msg.data = distance
+        distance_msg.data = self.distance
         self.publisher_.publish(distance_msg)
-        self.get_logger().info(f'Pose -> x: {self.x}, y: {self.y}, Distance: {distance:.2f}')
+        self.get_logger().info(f'Distance: {self.distance:.2f}')
 
 def main():
     rclpy.init()
